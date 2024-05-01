ab5af25be6b31741b6c3919597b4118b9c72d80e
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-05-01 13:21:01 -0400

    added notes

---
+++ main.js
@@ -0,0 +1,7 @@
+//It's about storing data and recallin git quickly. But like, it's a LOOOT of data
+
+//more specifically, it's about the relationship between all the data. The functions and operations that is applied to the data
+
+//an algorithm is a "step-by-step" approach where there is exactly one correct next step
+
+//there is a LOT to unpack here and to learn... all of this is used to make m code runfaster and better
